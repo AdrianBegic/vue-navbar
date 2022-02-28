0d8a87d05c7d34dce44ed5d99cdc9c015c52372b
Adrian Begic <adrianbegic1@gmail.com>
2022-02-28 16:15:16 -0500

    minor

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<Nav :style="options"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Nav from './components/nav.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        Nav,
+    },
+
+    data() {
+        return {
+          options: {
+
+            header: {
+                color: '#222',
+                fontSize: "1em",
+                backgroundColor: '#666',
+                padding: '10px'
+            },
+          },
+        }
+    },
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+    margin: 0;
+    padding: 0;
 }
 </style>
